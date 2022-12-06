65477f582f5b9554660de24355dac8030816f7f8
nishidemasami <nishidemasami@gmail.com>
2022-12-06 23:05:19 +0900

    fix design

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -18,7 +18,7 @@ const Main = (props: IMainProps) => (
 	<div className="antialiased w-full text-gray-700 md:px-0 sm:flex min-h-screen">
 		{props.meta}
 
-		<div className="mx-auto float-left px-3 w-full max-sm sm:w-3/4 bg-slate-100 min-h-fit sm:min-h-screen">
+		<div className="mx-auto float-left px-3 w-full sm:w-3/4 lg:w-[calc(100vw-256px)] bg-slate-100 min-h-fit sm:min-h-screen">
 			<div className="border-b border-gray-300 break-all text-center text-zinc-900 font-semibold block sm:hidden">
 				<div className="pt-4 pb-8">
 					<div className="font-semibold text-2xl text-gray-900">
@@ -43,7 +43,7 @@ const Main = (props: IMainProps) => (
 
 			<div className="text-base py-5">{props.children}</div>
 		</div>
-		<div className="w-full sm:w-1/4 float-right sm:sticky shadow-md sm:h-full sm:flex sm:top-auto sm:self-end sm:bottom-0 min-h-fit sm:min-h-screen">
+		<div className="w-full sm:w-1/4 lg:w-[256px] float-right sm:sticky shadow-md sm:h-full sm:flex sm:top-auto sm:self-end sm:bottom-0 min-h-fit sm:min-h-screen">
 			<Sidebar recents={props.recents} tags={props.tags} />
 		</div>
 	</div>
